443f232cc7d754f5b901312ebd246f2f15c404c1
flymarla <90324038+flymarla@users.noreply.github.com>
2022-09-21 06:03:19 -0500

    added test

--- _tests_/http_server.test.js
+++ _tests_/http_server.test.js
@@ -1,3 +1,15 @@
-import request from 'supertest';
-import express from 'express';
- 
+const express = require('express');
+const request = require('supertest');
+
+
+const app = express();
+app.use('/hello', function (req, res) {
+    res.send('hello world!');
+});
+
+describe('Hello Route Works', function () {
+    test('responst to /hello', async () => {
+        const res = await request(app).get('/hello');
+        expect (res.text).toEqual('hello world!');
+    });
+})
